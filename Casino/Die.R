46abfe6a84ece89c660d383bdea85f56878fc91a
DrinkMoreH2O <88058572+DrinkMoreH2O@users.noreply.github.com>
2022-07-03 14:47:16 -0700

    Add files via upload

--- Casino/Die.R
+++ Casino/Die.R
@@ -22,8 +22,3 @@ roll <- function(dice = 2){
   dice <- sample(die, dice, replace = TRUE, prob = c(1/8, 1/8, 1/8, 1/8, 1/8, 3/8))
   return(sum(dice))
 }
-
-rolls <- replicate(10000, roll())
-
-qplot(rolls, binwidth = 1)
-
